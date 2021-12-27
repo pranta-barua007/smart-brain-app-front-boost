44c99ac38b4d81d120d692465d4ce87a4231e921
pranta-barua007 <prantabarua247@gmail.com>
2021-12-28 00:59:06 +0600

    updates

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
-let SMART_BRAIN_API_URL = 'https://polar-waters-41859.herokuapp.com';
+let SMART_BRAIN_API_URL = 'https://smart-brain-backend-boost.herokuapp.com';
 
-if (process.env.NODE_ENV !== 'production') {
-    SMART_BRAIN_API_URL = 'http://localhost:5000'  
-};
+// if (process.env.NODE_ENV !== 'production') {
+//     SMART_BRAIN_API_URL = 'http://localhost:5000'  
+// };
 
 export default SMART_BRAIN_API_URL;
