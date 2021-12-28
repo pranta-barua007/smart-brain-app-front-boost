0306d8778d54f475e06644683e5c52831a62a33b
pranta-barua007 <prantabarua247@gmail.com>
2021-12-28 13:50:39 +0600

    updated

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -63,7 +63,7 @@ class Signin extends React.Component {
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
-            <form onSubmit={this.onSubmitSignIn} method="POST">
+            <form onSubmit={this.onSubmitSignIn}>
               <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                 <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                 <div className="mt3">
